36f1344db23a8be9d01664e5bb80bbb4cc171feb
wailovet <wailovet@163.com>
2019-01-16 20:57:01 +0800

    fix

--- src/struct/config.go
+++ src/struct/config.go
@@ -1,5 +1,12 @@
 package owstruct
 
+import (
+	"io/ioutil"
+	"os"
+	"log"
+	"encoding/json"
+)
+
 type Config struct {
 	Port        string `json:"port"`
 	Host        string `json:"host"`
@@ -8,5 +15,10 @@ type Config struct {
 }
 
 func (c *Config) ReadConfig(file string) {
-
+	configText, err := ioutil.ReadFile("./config/main.json")
+	if err != nil {
+		log.Println("配置文件错误,启动失败:", err.Error())
+		os.Exit(0)
+	}
+	json.Unmarshal(configText, *c)
 }
