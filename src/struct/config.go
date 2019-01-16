15b75d39cf0ab26e6ac002c59af423d733d7857b
wailovet <wailovet@163.com>
2019-01-16 20:55:09 +0800

    fix

--- src/struct/config.go
+++ src/struct/config.go
@@ -1,6 +1,12 @@
 package owstruct
 
 type Config struct {
-	Port string `json:"port"`
-	Host string `json:"host"`
+	Port        string `json:"port"`
+	Host        string `json:"host"`
+	CrossDomain string `json:"cross_domain"`
+	MaxMemory   int64  `json:"post_max_memory"`
+}
+
+func (c *Config) ReadConfig(file string) {
+
 }
