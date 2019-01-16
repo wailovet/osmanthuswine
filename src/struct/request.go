57ed199383c3df0606dfdaf48c154429328877d3
wailovet <wailovet@163.com>
2019-01-16 21:23:52 +0800

    fix

--- src/struct/request.go
+++ src/struct/request.go
@@ -1,6 +1,9 @@
 package owstruct
 
-import "mime/multipart"
+import (
+	"mime/multipart"
+	"net/http"
+)
 
 type Request struct {
 	GET     map[string]string
@@ -10,5 +13,26 @@ type Request struct {
 	SESSION map[string]string
 	HEADER  map[string]string
 	BODY    string
-	FILES    map[string][]*multipart.FileHeader
+	FILES   map[string][]*multipart.FileHeader
+}
+
+func (r *Request) SyncPostData(request *http.Request,mem int64) {
+	request.ParseForm()
+	request.ParseMultipartForm(mem)
+	r.POST = make(map[string]string)
+
+	post := request.PostForm
+	for k := range post {
+		r.POST[k] = request.PostFormValue(k)
+	}
+
+	if request.MultipartForm != nil {
+		r.FILES = request.MultipartForm.File
+		mf := request.MultipartForm.Value
+		for k := range mf {
+			if len(mf[k]) > 0 {
+				r.POST[k] = mf[k][0]
+			}
+		}
+	}
 }
