87403544e4cc100676d4cba0f6238c9b28b3301f
wailovet <wailovet@163.com>
2019-01-16 20:58:57 +0800

    fix

--- src/struct/config.go
+++ src/struct/config.go
@@ -11,7 +11,7 @@ type Config struct {
 	Port        string `json:"port"`
 	Host        string `json:"host"`
 	CrossDomain string `json:"cross_domain"`
-	MaxMemory   int64  `json:"post_max_memory"`
+	PMaxMemory   int64  `json:"post_max_memory"`
 }
 
 func (c *Config) ReadConfig(file string) {
