69884f5612a477cc8582abcb6e938e4f3e403b28
wailovet <wailovet@163.com>
2019-01-24 16:46:40 +0800

    fix

--- src/core/db.go
+++ src/core/db.go
@@ -3,14 +3,19 @@ package core
 import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
+	"github.com/jinzhu/gorm"
+	"github.com/go-errors/errors"
 )
 
 type Db struct {
 	XormEngine *xorm.Engine
+	GormDB     *gorm.DB
 }
 
-func CreateDbObject() (Db, error) {
+const DbTypeXorm = 0
+const DbTypeGorm = 1
 
+func CreateDbObject(dbtype int) (*Db, error) {
 	config := GetInstanceConfig()
 	mysqlConfig := mysql.NewConfig()
 	mysqlConfig.User = config.Db.User
@@ -19,10 +24,22 @@ func CreateDbObject() (Db, error) {
 	mysqlConfig.Params = config.Db.Params
 	mysqlConfig.Net = "tcp"
 	mysqlConfig.Addr = config.Db.Host + ":" + config.Db.Port
+
 	//helper.GetInstanceLog().Out(mysqlConfig.FormatDSN())
-	engine, err := xorm.NewEngine("mysql", mysqlConfig.FormatDSN())
-	ndb := Db{
-		XormEngine: engine,
+
+	if dbtype == DbTypeXorm {
+		engine, err := xorm.NewEngine("mysql", mysqlConfig.FormatDSN())
+		ndb := Db{
+			XormEngine: engine,
+		}
+		return &ndb, err
 	}
-	return ndb, err
+	if dbtype == DbTypeGorm {
+		db, err := gorm.Open("mysql", mysqlConfig.FormatDSN())
+		ndb := Db{
+			GormDB: db,
+		}
+		return &ndb, err
+	}
+	return nil, errors.New("error orm type")
 }
