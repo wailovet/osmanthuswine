e101dd2b7c9332a2d1371a772d34968406e7ae6a
wailovet <wailovet@163.com>
2019-01-24 20:26:04 +0800

    fix

--- src/core/db.go
+++ src/core/db.go
@@ -52,31 +52,35 @@ func GetThreadsConnectedNum() (int, error) {
 const DbTypeXorm = 0
 const DbTypeGorm = 1
 
-func CreateDbObject(dbtype int) (*Db, error) {
-	config := GetInstanceConfig()
-	mysqlConfig := mysql.NewConfig()
-	mysqlConfig.User = config.Db.User
-	mysqlConfig.DBName = config.Db.Name
-	mysqlConfig.Passwd = config.Db.Password
-	mysqlConfig.Params = config.Db.Params
-	mysqlConfig.Net = "tcp"
-	mysqlConfig.Addr = config.Db.Host + ":" + config.Db.Port
+var instanceDb *Db
 
-	if dbtype == DbTypeXorm {
-		engine, err := xorm.NewEngine("mysql", mysqlConfig.FormatDSN())
-		engine.SetMaxOpenConns(config.Db.MaxOpenConn)
-		ndb := Db{
-			XormEngine: engine,
+func CreateDbObject(dbtype int) (*Db, error) {
+	if instanceDb == nil {
+		config := GetInstanceConfig()
+		mysqlConfig := mysql.NewConfig()
+		mysqlConfig.User = config.Db.User
+		mysqlConfig.DBName = config.Db.Name
+		mysqlConfig.Passwd = config.Db.Password
+		mysqlConfig.Params = config.Db.Params
+		mysqlConfig.Net = "tcp"
+		mysqlConfig.Addr = config.Db.Host + ":" + config.Db.Port
+		if dbtype == DbTypeXorm {
+			engine, err := xorm.NewEngine("mysql", mysqlConfig.FormatDSN())
+			engine.SetMaxOpenConns(config.Db.MaxOpenConn)
+			instanceDb = &Db{
+				XormEngine: engine,
+			}
+			return instanceDb, err
 		}
-		return &ndb, err
-	}
-	if dbtype == DbTypeGorm {
-		db, err := gorm.Open("mysql", mysqlConfig.FormatDSN())
-		db.DB().SetMaxOpenConns(config.Db.MaxOpenConn)
-		ndb := Db{
-			GormDB: db,
+		if dbtype == DbTypeGorm {
+			db, err := gorm.Open("mysql", mysqlConfig.FormatDSN())
+			db.DB().SetMaxOpenConns(config.Db.MaxOpenConn)
+			instanceDb = &Db{
+				GormDB: db,
+			}
+			return instanceDb, err
 		}
-		return &ndb, err
 	}
+
 	return nil, errors.New("error orm type")
 }
