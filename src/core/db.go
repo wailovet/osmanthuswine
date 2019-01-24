6ab825e8c9a80b04500fa0c02c5c601c67f220ac
wailovet <wailovet@163.com>
2019-01-24 15:47:32 +0800

    fix

--- src/core/db.go
+++ src/core/db.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-sql-driver/mysql"
+	"github.com/wailovet/osmanthuswine/src/helper"
 )
 
 type Db struct {
@@ -20,7 +21,9 @@ func CreateDbObject() (Db, error) {
 	mysqlConfig.Passwd = config.Db.Password
 	mysqlConfig.Params = make(map[string]string)
 	mysqlConfig.Params["charset"] = config.Db.Charset
+	mysqlConfig.Net = "tcp"
 	mysqlConfig.Addr = config.Db.Host + ":" + config.Db.Port
+	helper.GetInstanceLog().Out(mysqlConfig.FormatDSN())
 	db, err := gorm.Open("mysql", mysqlConfig.FormatDSN())
 	ndb := Db{
 		GormDB: db,
