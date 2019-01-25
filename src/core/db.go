8f41fda1d7f888496d4a2bf1c76d8c4cc7cc550b
wailovet <wailovet@163.com>
2019-01-25 11:36:09 +0800

    fix

--- src/core/db.go
+++ src/core/db.go
@@ -4,48 +4,11 @@ import (
 	"github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
 	"github.com/jinzhu/gorm"
-	"database/sql"
-	"strconv"
 )
 
 type Db struct {
 	XormEngine *xorm.Engine
 	GormDB     *gorm.DB
-}
-
-var threadsConnectedNumConn *sql.DB
-
-func GetThreadsConnectedNum() (int, error) {
-	if threadsConnectedNumConn == nil {
-		cc := GetInstanceConfig()
-		mysqlConfig := mysql.NewConfig()
-		mysqlConfig.User = cc.Db.User
-		mysqlConfig.DBName = cc.Db.Name
-		mysqlConfig.Passwd = cc.Db.Password
-		mysqlConfig.Params = cc.Db.Params
-		mysqlConfig.Net = "tcp"
-		mysqlConfig.Addr = cc.Db.Host + ":" + cc.Db.Port
-		threadsConnectedNumConn, _ = sql.Open("mysql", mysqlConfig.FormatDSN())
-	}
-
-	rows, err := threadsConnectedNumConn.Query("show status like 'Threads_connected';")
-	if err != nil {
-		return 0, err
-	}
-	cols, err := rows.Columns()
-	if err != nil {
-		return 0, err
-	}
-	buff := make([]interface{}, len(cols)) // 临时slice，用来通过类型检查
-	data := make([]string, len(cols))      // 真正存放数据的slice
-	for i, _ := range buff {
-		buff[i] = &data[i] // 把两个slice关联起来
-	}
-	for rows.Next() {
-		rows.Scan(buff...)
-	}
-	ii, _ := strconv.Atoi(data[1])
-	return ii, nil
 }
 
 var instanceDb *Db
